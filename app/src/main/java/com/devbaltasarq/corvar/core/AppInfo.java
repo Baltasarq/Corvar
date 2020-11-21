8b377f0a33425ff56e887e8bbc35aac5945a3715
baltasar <baltasarq@gmail.com>
2020-11-21 19:52:11 +0100

    Applied the next gen formula Applied version info

--- app/src/main/java/com/devbaltasarq/corvar/core/AppInfo.java
+++ app/src/main/java/com/devbaltasarq/corvar/core/AppInfo.java
@@ -7,9 +7,9 @@ package com.devbaltasarq.corvar.core;
 /** The version information for this app. */
 public class AppInfo {
     public static final String NAME = "Corvar";
-    public static final String VERSION = "v0.0.3 20200901";
+    public static final String VERSION = "v0.0.4 20201121";
     public static final String AUTHOR = "MILE Group";
-    public static final String EDITION = "Selfsteem";
+    public static final String EDITION = "NextGen";
 
     public static String asShortString()
     {
